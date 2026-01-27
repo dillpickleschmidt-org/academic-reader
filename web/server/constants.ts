13345cdfa4c92e75aba7155afb0ea8528a3c1675
Dylan Kuzmick <drkuzmick@gmail.com>
2026-01-27 02:18:57 -0500

    increase max polling time

--- web/server/constants.ts
+++ web/server/constants.ts
@@ -1,5 +1,5 @@
 // Polling configuration
 export const POLLING = {
-  MAX_POLLS: 600, // 10 minutes at 1s intervals
+  MAX_POLLS: 1200, // 20 minutes at 1s intervals
   INTERVAL_MS: 1000,
 } as const
